84676275636d1f4883bac97502ac702d8da766e7
SathvikDixit <sathvikdixit21@gmail.com>
2025-09-04 12:16:19 +0530

    Succedded in installing tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='p-10'>
+      <button className='px-3 py-1 bg-sky-400 text-white rounded-md'>Button</button>
+    </div>
+  )
+}
+
+export default App
