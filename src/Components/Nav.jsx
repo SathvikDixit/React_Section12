03facc627f37e81608d07b63f067bbc0ddf027e0
SathvikDixit <sathvikdixit21@gmail.com>
2025-09-04 21:21:55 +0530

    qwreqwer

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 
 const Nav = () => {
   return (
     <div>
       <nav className="p-5 flex justify-center gap-10">
-        <Link to="/">Home</Link>
-        <Link to="/user">User</Link>
-        <Link to="/about">About</Link>
+        <NavLink 
+            style={
+                (e) => {
+                    return{
+                        color: e.isActive? "tomato" : "",
+                    }
+                }
+            }
+        to="/">Home</NavLink>
+        <NavLink to="/user">User</NavLink>
+        <NavLink to="/about">About</NavLink>
       </nav>
     </div>
   );
